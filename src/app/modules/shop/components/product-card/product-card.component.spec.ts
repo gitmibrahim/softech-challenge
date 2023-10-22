9668143431111111e448bd1833329cef42f2c2cf
gitmibrahim <muhammed.ibrahim.me@gmail.com>
2023-10-22 22:29:22 +0300

    #3 _Create_ product card component TDD

--- src/app/modules/shop/components/product-card/product-card.component.spec.ts
+++ src/app/modules/shop/components/product-card/product-card.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductCardComponent } from './product-card.component';
+import { CartServiceMock, ProductMock } from '../../utilities/test-mocks';
+import { By } from '@angular/platform-browser';
+import { CartService } from '../../services/cart.service';
 
 describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
@@ -8,7 +11,10 @@ describe('ProductCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductCardComponent]
+      imports: [ProductCardComponent],
+      providers: [
+        { provide: CartService, useValue: CartServiceMock }
+      ]
     })
     .compileComponents();
     
@@ -19,5 +25,44 @@ describe('ProductCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have product input', () => {
+    component.product = ProductMock
+    expect(component.product.ProductId).toEqual(ProductMock.ProductId)
+  });
+
+  it('should have add to cart button', () => {
+    const addToCartBtn = fixture.nativeElement.query(By.css('button.add-to-cart'))
+    expect(addToCartBtn).toBeTruthy()
+  });
+
+  it('should add product to cart', async () => {
+    spyOn(component, 'addProductToCart')
+
+    const addToCartBtn = fixture.nativeElement.query(By.css('button.add-to-cart'))
+    addToCartBtn.click(ProductMock)
+
+    await fixture.whenStable()
+    
+    expect(component.addProductToCart).toHaveBeenCalledOnceWith(ProductMock);
+    expect(CartServiceMock.addProductToCart).toHaveBeenCalledOnceWith(ProductMock)
+  });
+
+  it('should edit product quantity', async () => {
+    spyOn(component, 'editProductQuantity')
+    expect(component.quantity).toEqual(0)
+
+    const addToCartBtn = fixture.nativeElement.query(By.css('button.add-to-cart'))
+    addToCartBtn.click(ProductMock)
+    await fixture.whenStable()
+    expect(component.quantity).toEqual(1)
+
+    const plusBtn = fixture.nativeElement.query(By.css('button.plus'))
+    plusBtn.click(ProductMock)
+    await fixture.whenStable()
+
+    expect(component.editProductQuantity).toHaveBeenCalledOnceWith(ProductMock, 'add');
+    expect(CartServiceMock.editProductQuantity).toHaveBeenCalledOnceWith(ProductMock, 'add')
   });
 });
