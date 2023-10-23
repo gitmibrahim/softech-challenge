39ba6b5906d9d77f9983ea0b4e19acfdf202fff9
gitmibrahim <muhammed.ibrahim.me@gmail.com>
2023-10-23 20:21:10 +0300

    #14 Create product list cmp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => 
+      import('./modules/shop/components/product-list/product-list.component').then(
+        (c) => c.ProductListComponent
+      )
+  }
+];
